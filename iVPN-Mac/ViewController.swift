d827b8660a7d55b3e60913a68cab60e15d670bbd
Steven <qzs2147@gmail.com>
2015-11-05 11:47:40 +0800

    Initial Commit

---
+++ iVPN-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iVPN-Mac
+//
+//  Created by Steven on 15/11/5.
+//  Copyright © 2015年 Neva. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
